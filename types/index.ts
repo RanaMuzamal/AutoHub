0c6998a67f1fa9aad6ed80b2d20ad2dc6c2cc0a2
Rana Muzamil <muzamilrana149@gmail.com>
2023-06-18 17:29:32 +0500

    add types for button component

---
+++ types/index.ts
@@ -0,0 +1,7 @@
+import { MouseEventHandler } from "react";
+
+export interface CustomButton {
+    title: string;
+    containerStyles?: string;
+    handleClick?: MouseEventHandler<HTMLButtonElement>;
+}
