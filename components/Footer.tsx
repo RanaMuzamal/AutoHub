9f09c21b47174a59be86347707a41b5c6822b87e
Rana Muzamil <muzamilrana149@gmail.com>
2023-06-25 10:48:38 +0500

    complete footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,57 @@
+import { footerLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function Footer() {
-    return <div>Footer</div>;
+    return (
+        <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
+            <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+                <div className="flex flex-col justify-start items-start gap-6">
+                    <Image
+                        src="/logo-no-background.png"
+                        alt="AutoHub Logo"
+                        width={168}
+                        height={38}
+                        className="object-contain"
+                    />
+                    <p className="text-base text-gray-700">
+                        AutoHub 2023
+                        <br />
+                        All Rights Reserved &copy;
+                    </p>
+                </div>
+                <div className="footer__links">
+                    {footerLinks.map((link) => (
+                        <div key={link.title} className="footer__link">
+                            <h3 className="font-bold">{link.title}</h3>
+                            {link.links.map((item) => (
+                                <Link
+                                    key={item.title}
+                                    href={item.url}
+                                    className="text-gray-500"
+                                >
+                                    {item.title}
+                                </Link>
+                            ))}
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+                <p>@2023 AutoHub. All Rights Reserved!</p>
+                <div className="footer__copyrights-link mr-2">
+                    <Link href="/" className="text-gray-500">
+                        Privacy Policy
+                    </Link>
+                    {""}
+                    <Link href="/" className="text-gray-500">
+                        Terms of Use
+                    </Link>
+                </div>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
