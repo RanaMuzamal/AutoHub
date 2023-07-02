09f8edb645c2c452c69583f399acd58a94ae1469
Rana Muzamil <muzamilrana149@gmail.com>
2023-07-02 05:51:38 +0500

    add components in car catalogue

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,7 @@
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
 
-export { Hero, Navbar, Footer };
+export { Hero, Navbar, Footer, SearchBar, CustomFilter };
