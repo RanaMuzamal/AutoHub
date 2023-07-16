f3454fa59793730be12892bded9f1c154ff6e82b
Rana Muzamil <muzamilrana149@gmail.com>
2023-07-16 11:33:23 +0500

    add search icon

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,18 +1,53 @@
 "use client";
 import React, { useState } from "react";
 import SearchManufacturer from "./SearchManufacturer";
+import Image from "next/image";
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+    <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+        <Image
+            src="/magnifying-glass.svg"
+            alt="magnifying glass"
+            width={40}
+            height={40}
+            className="object-contain"
+        />
+    </button>
+);
 
 function SearchBar() {
     const [manufacturer, setManufacturer] = useState("");
+    const [model, setModel] = useState("");
     const handleSearch = () => {};
+
     return (
         <form className="searchbar" onSubmit={handleSearch}>
             <div className="searchbar__item">
                 <SearchManufacturer
-                    setManufacture={setManufacturer}
+                    setManufacturer={setManufacturer}
                     manufacturer={manufacturer}
                 />
+                <SearchButton otherClasses="sm:hidden" />
             </div>
+            <div className="searchbar__item">
+                <Image
+                    src="/model-icon.png"
+                    width={25}
+                    height={25}
+                    className="absolute w-[20px] h-[20px] ml-4"
+                    alt="car model"
+                />
+                <input
+                    type="text"
+                    name="model"
+                    value={model}
+                    onChange={(e) => setModel(e.target.value)}
+                    placeholder="Tiguan"
+                    className="searchbar__input"
+                />
+                <SearchButton otherClasses="sm:hidden" />
+            </div>
+            <SearchButton otherClasses="max-sm:hidden" />
         </form>
     );
 }
