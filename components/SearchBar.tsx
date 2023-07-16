f2d0b084fa74bbd775aa84939c60523763bf6430
Rana Muzamil <muzamilrana149@gmail.com>
2023-07-16 11:59:58 +0500

    complete handleseach button functionality

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import SearchManufacturer from "./SearchManufacturer";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
     <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
@@ -18,8 +19,38 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
 function SearchBar() {
     const [manufacturer, setManufacturer] = useState("");
     const [model, setModel] = useState("");
-    const handleSearch = () => {};
+    const router = useRouter();
 
+    const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (manufacturer === "" || model === "") {
+            return alert("Please fill the search bar first!");
+        }
+
+        updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
+    };
+
+    const updateSearchParams = (model: string, manufacturer: string) => {
+        const searchParams = new URLSearchParams(window.location.search);
+
+        if (manufacturer) {
+            searchParams.set("manufacturer", manufacturer);
+        } else {
+            searchParams.delete("manufacturer");
+        }
+        if (model) {
+            searchParams.set("model", model);
+        } else {
+            searchParams.delete("model");
+        }
+
+        const newPathname = `${
+            window.location.pathname
+        }?${searchParams.toString()}`;
+
+        router.push(newPathname);
+    };
     return (
         <form className="searchbar" onSubmit={handleSearch}>
             <div className="searchbar__item">
@@ -31,7 +62,7 @@ function SearchBar() {
             </div>
             <div className="searchbar__item">
                 <Image
-                    src="/model-icon.png"
+                    src="/model.png"
                     width={25}
                     height={25}
                     className="absolute w-[20px] h-[20px] ml-4"
